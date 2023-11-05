97bfb689d616fd0a3374fc539a1facfb8bef82be
Amin7775 <amin.dpi24@gmail.com>
2023-11-06 00:16:23 +0600

    Handle Cors issue and Get jwt token on client side

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -27,16 +27,21 @@ const AuthProvider = ({ children }) => {
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            const userEmail = currentUser?.email || user?.email;
+            const loggedUser = {email: userEmail}
             setUser(currentUser);
             console.log('current user', currentUser);
             setLoading(false);
+
             //if user exist then issue a token
             if(currentUser){
-                const loggedUser = {email: currentUser.email}
                 axios.post("http://localhost:5000/jwt",loggedUser,{withCredentials:true})
                 .then(res=> {
                     console.log(res.data)
                 })
+            }else{
+                axios.post('http://localhost:5000/logout', loggedUser,{withCredentials:true})
+                .then(res => console.log(res.data))
             }
         });
         return () => {
